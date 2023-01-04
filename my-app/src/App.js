e4b3fa7ce42fd41fee32ec6a514da1899a7ca45b
Priyanka9120 <priyanka912066@gmail.com>
2023-01-04 22:28:10 +0530

    React Assignment2

---
+++ my-app/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      showParagraph: false,
+    };
+  }
+  render() {
+   
+    return (
+      <>
+        <textarea
+          value={this.state.input}
+          onChange={(e) => this.setState({ input: e.target.value })}
+        />
+        <br />
+        <button
+          onClick={() => {
+            this.setState({
+              showParagraph: !this.state.showParagraph,
+            });
+          }}
+        >
+          Display in uppercase
+        </button>
+        
+      </>
+    );
+  }
+}     
+      
+  
+
+export default App;
